e62006fa0c58850176494e57dcf6058a66ac718c
byron2121 <88109083+byron2121@users.noreply.github.com>
2021-08-02 10:59:38 -0500

    Update exppresiones_avanzadas.sql
    IFNULL: se crea una  columna llamada numero_contacto, la cual será  iguala la columna telefono o la frase 'no tiene celular' en caso de que la columna teléfono esté vacía

--- exppresiones_avanzadas.sql
+++ exppresiones_avanzadas.sql
@@ -1,5 +1,4 @@
 #IF
-
 select nombre_empleado,
 if (fecha_ingreso < '2018-12-31', concat(id_empleado, '018'),
 	if (fecha_ingreso < '2019-12-31', concat(id_empleado, '019'),
@@ -9,4 +8,10 @@ if (fecha_ingreso < '2018-12-31', concat(id_empleado, '018'),
 		)
 	)
 as nuevo_codigo
+from base_uno.empleado;
+
+
+#IFNULL
+select nombre_empleado,
+ifnull(telefono,'no tiene celular') as numero_contacto
 from base_uno.empleado;
